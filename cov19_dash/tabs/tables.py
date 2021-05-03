649b4b59ee2515d65d26ba4840af91f8382d3c89
Tim-Abwao <abwaomusungu@gmail.com>
2021-05-03 23:58:51 +0300

    Add button to enable data download

--- cov19_dash/tabs/tables.py
+++ cov19_dash/tabs/tables.py
@@ -1,7 +1,10 @@
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
-from cov19_dash.data import load_latest_day_data
+from cov19_dash.dash_app import app
+from cov19_dash.data import load_latest_day_data, load_time_series_data
+from dash.dependencies import Input, Output
+
 
 data = load_latest_day_data()
 dates = data.pop('Date')
@@ -21,11 +24,27 @@ The data used here is obtained from the [JHU CSSE COVID-19 Data][1]
 [2]: https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_\
 data/csse_covid_19_time_series
 """),
-    dash_table.DataTable(
-        id='table',
-        columns=[{"name": col, "id": col}for col in data.columns],
-        data=data.to_dict('records'),
-        sort_action='native',
-        page_size=50,
-    )
+    html.Div(className='raw-data-table', children=[
+        dash_table.DataTable(
+            id='table',
+            columns=[{'name': col, 'id': col}for col in data.columns],
+            data=data.to_dict('records'),
+            sort_action='native',
+            page_size=50,
+        )
+    ]),
+    html.Div(style={'margin': '5%'}, children=[
+        html.Button('Download CSV', id='btn_csv'),
+        dcc.Download(id='download-dataset', type='text/csv'),
+    ])
 ])
+
+
+@app.callback(
+    Output('download-dataset', 'data'),
+    Input('btn_csv', 'n_clicks'),
+    prevent_initial_call=True,
+)
+def download_global_dataset(n_clicks):
+    data = load_time_series_data()
+    return dcc.send_data_frame(data.to_csv, 'covid19-global.csv')
