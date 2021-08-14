979c581391461fe773ab53e499cc51beb4a1fac6
Tim-Abwao <abwaomusungu@gmail.com>
2021-08-14 22:48:33 +0300

    Switch Our World in Data repo
    Use the 'owid / covid-19-data' repository as the primary data source. It has cleaner and more exhaustive info.
    Use the JHU CSSE for time series data.

--- cov19_dash/data.py
+++ cov19_dash/data.py
@@ -1,26 +1,55 @@
 from pathlib import Path
-
 import pandas as pd
 
-BASE_URL = (
-    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/"
-    "csse_covid_19_data/csse_covid_19_time_series/time_series_covid19"
+OWID_URL = (
+    "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/"
+    "latest/owid-covid-latest.csv"
 )
 
-data_dir = Path("data")
-location_data = pd.read_csv(data_dir.joinpath("location_data.csv"))
+JHU_URL = (
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_"
+    "covid_19_data/csse_covid_19_time_series/time_series_covid19"
+)
+
+DATA_DIR = Path("covid-19-data")
+DATA_DIR.mkdir(exist_ok=True)
 
 
-def fetch_data(category="confirmed"):
+def fetch_latest_data() -> None:
+    """Get global covid-19 data from the "Our World in Data" public GitHub
+    repository.
+    """
+    print("Fetching latest data...")
+    data = pd.read_csv(OWID_URL)
+
+    # Switch column names to title case
+    data.columns = [col.replace("_", " ").title() for col in data.columns]
+
+    (
+        # Remove regional totals: 'OWID_AFR', 'OWID_ASI', 'OWID_EUR',
+        # 'OWID_EUN', 'OWID_INT', 'OWID_KOS', 'OWID_NAM', 'OWID_OCE',
+        # 'OWID_SAM', 'OWID_WRL'
+        data[~data["Iso Code"].str.startswith("OWID")]
+        # Persist local copy
+        .to_csv(DATA_DIR / "latest-data.csv", index=False)
+    )
+
+
+def fetch_jhu_data(category: str) -> pd.Series:
     """Get global covid-19 data for the given category from the JHU CSSE
     COVID-19 repository.
 
     Parameters
     ----------
-    category : {"confirmed", "deaths", "recovered"}
+    category : {"confirmed", "deaths"}
         The information to fetch.
+
+    Returns
+    -------
+    pd.Series
+        Data for the specified category.
     """
-    data = pd.read_csv(f"{BASE_URL}_{category}_global.csv")
+    data = pd.read_csv(f"{JHU_URL}_{category}_global.csv")
 
     data = (
         # Eliminate unnecessary columns
@@ -33,118 +62,68 @@ def fetch_data(category="confirmed"):
         # Pivot the index. This results in a Series with the category's data,
         # and a MultiIndex with "Country/Region" and "Date".
         .unstack()
-        # Ensure all values are positive
-        .clip(lower=0)
         # Set the category as the Series' label.
         .rename(category.capitalize())
     )
-
     return data
 
 
-def get_case_information():
-    """Get combined "confirmed", "deaths" and "recovered" case information.
-    Then calculate "active" cases.
+def fetch_time_series_data() -> None:
+    """Get "confirmed" and "deaths" information.
 
     Returns
     -------
     A pandas DataFrame with covid-19 case information.
     """
-    case_data = [
-        fetch_data(category)
-        for category in {"confirmed", "deaths", "recovered"}
-    ]
-    case_data_df = (
-        # Combine the case data into a DataFrame
-        pd.concat(case_data, axis=1)
-        # Restore "Country/Region" and "Date" as columns, and set a default
-        # RangeIndex
-        .reset_index()
-    )
-    # Convert the dates to datetime format
-    case_data_df["Date"] = pd.to_datetime(case_data_df["Date"])
-
-    # Calculate active cases
-    case_data_df["Active"] = (
-        case_data_df["Confirmed"]
-        - case_data_df["Recovered"]
-        - case_data_df["Deaths"]
-    )
-    return case_data_df
-
-
-def refresh_datasets():
-    """Save fresh copies of the data used in the dashboard."""
-    case_data = get_case_information()
-
-    # Save daily time series
-    time_series_file = data_dir.joinpath("time_series.csv")
-    case_data.to_csv(time_series_file, index=False)
-
-    # Save latest day's data
-    latest_day_file = data_dir.joinpath("latest_day.csv")
-    (
-        # Select latest day's data
-        case_data.query("Date == @case_data['Date'].max()").to_csv(
-            latest_day_file, index=False
-        )
+    print("Fetching time series info...")
+    case_data = pd.concat(
+        [fetch_jhu_data(category) for category in ("confirmed", "deaths")],
+        axis=1,
     )
 
+    # Restore "Country/Region" and "Date" index levels as columns, and set a
+    # default RangeIndex
+    case_data.reset_index(inplace=True)
 
-def load_latest_day_data():
-    """Get data for the latest day, and include location data.
+    # Harmonize country names accross data sources
+    case_data["Country/Region"] = case_data["Country/Region"].map(
+        {
+            "Cabo Verde": "Cape Verde",
+            "Congo (Brazzaville)": "Congo",
+            "Congo (Kinshasa)": "Democratic Republic of Congo",
+            "Micronesia": "Micronesia (country)",
+            "Burma": "Myanmar",
+            "West Bank and Gaza": "Palestine",
+            "Korea, South": "South Korea",
+            "Taiwan*": "Taiwan",
+            "Timor-Leste": "Timor",
+            "US": "United States",
+            "Holy See": "Vatican",
+        }
+    )
+
+    case_data.to_csv(DATA_DIR / "time-series-data.csv", index=False)
+
+
+def load_latest_day_data() -> pd.DataFrame:
+    """Get cleaned COVID-19 data for the latest day.
 
     Returns
     -------
-    A pandas DataFrame with "confirmed", "active", "recovered" and "deaths"
-    info for the latest day.
+    pandas.DataFrame
+        COVID-19 info for the latest day.
     """
-    file = data_dir.joinpath("latest_day.csv")
-    if file.exists():
-        data = pd.read_csv(file, parse_dates=["Date"])
-        return data.merge(location_data)
-    else:
-        print("Downloading data...")
-        refresh_datasets()
-        return load_latest_day_data()
+    return pd.read_csv(
+        DATA_DIR / "latest-data.csv", parse_dates=["Last Updated Date"]
+    )
 
 
-def load_time_series_data():
-    """Get daily time series data.
+def load_time_series_data() -> pd.DataFrame:
+    """Get cleaned COVID-19 time series data.
 
     Returns
     -------
-    A pandas DataFrame with "confirmed", "recovered" and "deaths" info over a
-    range of dates.
+    pandas.DataFrame
+        COVID-19 time series data.
     """
-    file = data_dir.joinpath("time_series.csv")
-    if file.exists():
-        return pd.read_csv(file, parse_dates=["Date"])
-    else:
-        print("Downloading data...")
-        refresh_datasets()
-        return load_time_series_data()
-
-
-def check_if_data_is_stale():
-    """Scan current files, and create fresh copies if they are missing or
-    outdated.
-    """
-    file = data_dir.joinpath("latest_day.csv")
-
-    if file.exists():
-        # Read a portion of the file to get date info
-        data = pd.read_csv(file, nrows=3, parse_dates=["Date"])
-        latest_date = data["Date"].max()
-        time_now = pd.to_datetime("now")
-
-        if (time_now - latest_date).days > 1:
-            # Since the data source is updated once a day with data for the
-            # previous day, the gap in days could get to 2, but only for a
-            # few hours.
-            print("Fetching fresh data...")
-            refresh_datasets()
-    else:
-        # Download fresh copies if the file is missing
-        print("Downloading data...")
-        refresh_datasets()
+    return pd.read_csv(DATA_DIR / "time-series-data.csv", parse_dates=["Date"])
