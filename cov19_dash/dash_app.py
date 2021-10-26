313d816aacfa6a71ee7cda6024a89315c7e5433e
Tim-Abwao <abwaomusungu@gmail.com>
2021-10-26 13:56:46 +0300

    Load plotly script from cdn
    Loading the script from the waitress server was laggy since resources
    are limited, and the file wasn't being compressed.

--- cov19_dash/dash_app.py
+++ cov19_dash/dash_app.py
@@ -1,15 +1,18 @@
 import dash
 
 app = dash.Dash(
-    "cov19_dash",
-    suppress_callback_exceptions=True,
+    __name__,
     title="COVID-19 Dashboard",
+    external_scripts=[
+        "https://cdn.plot.ly/plotly-2.5.1.min.js",
+    ],
     meta_tags=[
         {
             "name": "viewport",
             "content": "width=device-width, initial-scale=1.0",
         }
     ],
+    suppress_callback_exceptions=True,
 )
 
 server = app.server
