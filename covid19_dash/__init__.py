e632ac72a95e0fde98cc9402eba1da905c102821
Tim-Abwao <abwaomusungu@gmail.com>
2022-08-29 12:54:27 +0300

    Cut redundant function 'run_server'

--- covid19_dash/__init__.py
+++ covid19_dash/__init__.py
@@ -1,13 +1,8 @@
 import logging
 
-import waitress
-
 from covid19_dash.dash_app import app
 
 # Set waitress.queue logging level to ERROR
 logging.getLogger("waitress.queue").setLevel(logging.ERROR)
 
-
-def run_server() -> None:
-    """Start the dashboard server."""
-    waitress.serve(app.server, host="localhost", port=8000)
+server = app.server
